0a1e8ace94546cc917367d610fb3705ccb011342
Tarek <mbpr@TPT.local>
2018-04-19 20:29:07 +0300

    changed directory, added assignment 3 files

---
+++ Assignment 3/land.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <var.h>
+class LOr;
+class LNot;
+
+struct LAnd {
+
+  //////// replace all these with leftPointer & rightPointer only
+
+  Var * leftV;
+  Var * rightV;
+  LAnd * leftAnd;
+  LAnd * rightAnd;
+  LOr * leftOr;
+  LOr * rightOr;
+  LNot * leftNot;
+  LNot * rightNot;
+  ///////// remove left & right since they are only used for typecasting, and one shouldn't need to specify their type (error prone)
+  LAnd(void * left, void * right, LType lType, LType rType);  //ptr to left exp, ptr to right exp, type of left exp, type of right exp
+  TriValue evaluate();
+};
+
+ostream & operator << (ostream & os, LAnd & v);
